cdd5b02ac85c708871459263254256c853c18274
pm <parancs.mate@gmail.com>
2026-07-01 14:05:17 +0200

    .

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from docker-workshop-data-engineering!")
+
+
+if __name__ == "__main__":
+    main()
--- test/pipeline/ingest_data.py
+++ test/pipeline/ingest_data.py
@@ -7,6 +7,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 # In[2]:
 
 
@@ -45,27 +46,32 @@ parse_dates = [
 
 # In[22]:
 
-def run():
-    year=2021
+
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL user')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+
+
+    year=2019
     month=1
-    pg_user='root'
-    pg_password='root'
-    pg_host='localhost'
-    pg_db='ny_taxi'
-    pg_port=5432
+    
     chunksize=10000
-    target_table="yellow_taxi_data"
 
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-    url=f'{prefix}yellow_tripdata_{year}_{month:02d}.csv.gz'
+    url=f'{prefix}yellow_tripdata_{year}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_password}@{pg_host}:{pg_port}/{pg_db}')
+    engine = create_engine(f'postgresql+psycopg://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
 
 
     df_iter = pd.read_csv(
-        prefix + 'yellow_tripdata_2021-01.csv.gz',
+        url,
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
